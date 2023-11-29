a9f9cea0bc8019771ffe4298deb50a87c27a27b0
Neto Polacchini <netopolacchini@gmail.com>
2023-11-28 22:16:06 -0300

    fresh start

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Startando Api');
+});
