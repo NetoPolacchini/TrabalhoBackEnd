006c374cf85afaeab31e61204f2e94600f4fdca6
Neto Polacchini <64282566+NetoPolacchini@users.noreply.github.com>
2023-12-03 22:58:23 -0300

    HallApi created

--- helpers/mongo.js
+++ helpers/mongo.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 module.exports = (req, res, next) => {
     mongoose.connect("mongodb://127.0.0.1:27017/BackEnd").catch((err) => {
-        console.log("Error ao conectar no banco...")
+        console.log("Error ao conectar no banco...", err)
     })
     return next()
 }
