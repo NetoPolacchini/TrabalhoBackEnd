42e0e27e5336d46bd2007bd8a2852165d46baa46
Neto Polacchini <64282566+NetoPolacchini@users.noreply.github.com>
2023-12-08 19:21:00 -0300

    arquivo .env adicionado

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.Port || 3000;
 const session = require("express-session")
 const cookieParser = require('cookie-parser')
 
+app.use(require('./helpers/mongo'))
 app.use(cookieParser())
 app.use(session({
     secret:'Neto',
@@ -11,9 +13,7 @@ app.use(session({
     saveUninitialized: false
 }))
 app.use(express.json());
-app.use(require('./helpers/mongo'))
 app.use("/install", require("./Script/script"))
-
 app.use("/hall", require("./control/HallAPI"))
 app.use("/user", require("./control/UserAPI"))
 app.use("/buffet", require("./control/BuffetAPI"))
--- helpers/mongo.js
+++ helpers/mongo.js
@@ -1,8 +1,11 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const mongoURI = process.env.MONGO_URI;
 
 module.exports = (req, res, next) => {
-    mongoose.connect("mongodb://127.0.0.1:27017/BackEnd").catch((err) => {
-        console.log("Error ao conectar no banco:", err)
-    })
-    return next()
-}
+    mongoose.connect(mongoURI).catch((err) => {
+        console.log("Erro ao conectar no banco:", err);
+    });
+    return next();
+};
