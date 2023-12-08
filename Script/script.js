9a237f0b8a6eb28d445ab069131c9108e69e5e7a
Neto Polacchini <64282566+NetoPolacchini@users.noreply.github.com>
2023-12-08 20:39:18 -0300

    rota /install/ configurada para popular o banco de dados

--- Script/script.js
+++ Script/script.js
@@ -11,10 +11,10 @@ router.get('/', async(req, res)=>{
 
         const predefinedUsers = [
             { nome: 'Admin', email: 'admin@gmail.com', senha: 'senha123', isAdmin: true },
-            { nome: 'Usuário1', email: 'usuario1@gmail.com', senha: 'senha123', isAdmin: false },
-            { nome: 'Usuário2', email: 'usuario2@gmail.com', senha: 'senha123', isAdmin: false },
-            { nome: 'Usuário3', email: 'usuario3@gmail.com', senha: 'senha123', isAdmin: false },
-            { nome: 'Usuário4', email: 'usuario4@gmail.com', senha: 'senha123', isAdmin: false }
+            { nome: 'João', email: 'joao@gmail.com', senha: 'senha123', isAdmin: false },
+            { nome: 'Isabela', email: 'isabela@gmail.com', senha: 'senha123', isAdmin: false },
+            { nome: 'Karine', email: 'karine@gmail.com', senha: 'senha123', isAdmin: false },
+            { nome: 'Leonardo', email: 'leonardo@gmail.com', senha: 'senha123', isAdmin: false }
         ];
 
         for (const user of predefinedUsers) {
@@ -23,11 +23,11 @@ router.get('/', async(req, res)=>{
 
 
         const predefinedHalls = [
-            {nome:"Hall1", endereco:{rua:"Rau1", numero:"1000", bairro:"bairro1", cidade:"cidade1", cep:"cep1"},capacidadeMaxima:120, preco: 890},
-            {nome:"Hall2", endereco:{rua:"Rau2", numero:"1002", bairro:"bairro2", cidade:"cidade2", cep:"cep2"},capacidadeMaxima:50, preco: 650},
-            {nome:"Hall3", endereco:{rua:"Rau3", numero:"1003", bairro:"bairro3", cidade:"cidade3", cep:"cep3"},capacidadeMaxima:340, preco: 1120},
-            {nome:"Hall4", endereco:{rua:"Rau4", numero:"1004", bairro:"bairro4", cidade:"cidade4", cep:"cep4"},capacidadeMaxima:90, preco: 900},
-            {nome:"Hall5", endereco:{rua:"Rau5", numero:"1005", bairro:"bairro5", cidade:"cidade5", cep:"cep5"},capacidadeMaxima:76, preco: 450}
+            {nome:"Celebração Palace", endereco:{rua:"Rau1", numero:"1000", bairro:"bairro1", cidade:"cidade1", cep:"cep1"},capacidadeMaxima:120, preco: 890},
+            {nome:"Elegância Eventos", endereco:{rua:"Rau2", numero:"1002", bairro:"bairro2", cidade:"cidade2", cep:"cep2"},capacidadeMaxima:50, preco: 650},
+            {nome:"Espaço Festivo Glamour", endereco:{rua:"Rau3", numero:"1003", bairro:"bairro3", cidade:"cidade3", cep:"cep3"},capacidadeMaxima:340, preco: 1120},
+            {nome:"Salão do Sonho", endereco:{rua:"Rau4", numero:"1004", bairro:"bairro4", cidade:"cidade4", cep:"cep4"},capacidadeMaxima:90, preco: 900},
+            {nome:"Jardim das Festas", endereco:{rua:"Rau5", numero:"1005", bairro:"bairro5", cidade:"cidade5", cep:"cep5"},capacidadeMaxima:76, preco: 450}
         ];
 
         for (const hall of predefinedHalls) {
@@ -35,18 +35,18 @@ router.get('/', async(req, res)=>{
         }
 
         const predefinedBuffets = [
-            {nome:"Buffet1", tipoComida:"Japonesa"},
-            {nome:"Buffet2", tipoComida:"Italiana"},
-            {nome:"Buffet3", tipoComida:"Lanches"},
-            {nome:"Buffet4", tipoComida:"Pastéis"},
-            {nome:"Buffet5", tipoComida:"Frutos do Mar"},
+            {nome:"Delícias Gourmet", tipoComida:"Cozinha Internacional"},
+            {nome:"Sabor & Arte Buffet", tipoComida:"Comida Contemporânea"},
+            {nome:"Festa Italiana Catering", tipoComida:"Italiana"},
+            {nome:"Sabores do Oriente Eventos", tipoComida:"Culinária Asiática"},
+            {nome:"Churrasco Brasileiro Catering", tipoComida:"Churrasco Brasileiro"},
         ];
 
         for (const buffet of predefinedBuffets) {
             await BuffetDAO.save(buffet.nome, buffet.tipoComida);
         }
 
-        res.json(sucess('Contas criadas com sucesso!'));
+        res.json(sucess('Banco de dados populado com sucesso!'));
     } catch (err) {
         res.json(fail(err.message));
     }
